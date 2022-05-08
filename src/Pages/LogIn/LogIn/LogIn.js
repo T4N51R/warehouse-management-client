7bcafc62e88441d99f394770c6cb6804593c06c9
Tanvir Ahmed <ahmedtanvir290@gmail.com>
2022-05-09 03:48:32 +0600

    Log In code added

--- src/Pages/LogIn/LogIn/LogIn.js
+++ src/Pages/LogIn/LogIn/LogIn.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init'
 const LogIn = () => {
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const navigate = useNavigate();
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+
+    if (user) {
+        navigate('/')
+    }
+    const handleLogIn = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        signInWithEmailAndPassword(email, password);
+    }
+
     return (
         <div className='container fs-3'>
             <h1 className='text-primary text-center'>Please LogIn</h1>
-            <Form>
+            <Form onSubmit={handleLogIn}>
                 <Form.Group className="mb-3 w-75 mx-auto" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
                 </Form.Group>
                 <Form.Group className="mb-3 w-75 mx-auto" controlId="formBasicEmail">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Enter Password" />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Enter Password" />
                 </Form.Group>
                 <Button className="w-50 mx-auto mb-3 d-block py-2 fs-4" variant="primary" type="submit">
                     Submit
